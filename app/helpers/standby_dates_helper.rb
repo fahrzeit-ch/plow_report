f61b8a48feae4a4e339f0c24e9977fa1c4fc40dc
Stefan Staub <ste.staub@gmail.com>
2018-06-04 23:18:04 +0200

    added more colors to identicate drivers

--- app/helpers/standby_dates_helper.rb
+++ app/helpers/standby_dates_helper.rb
@@ -1,9 +1,9 @@
 module StandbyDatesHelper
 
-  COLOR_MAP = %w(#00AEFF #ff7319 #f0ad4e #FF7518 #3FB618 #20c997 #9954BB #868e96)
+  COLOR_MAP = %w(#fff #868e96 #373a3c #000 #2780E3 #6610f2 #613d7c #e83e8c #FF0039 #f0ad4e #FF7518 #3FB618 #bcffc5 #20c997 #9954BB #8A4117 #C36241)
 
   def driver_color(driver)
-    COLOR_MAP[driver.id % 8]
+    COLOR_MAP[driver.id % 17]
   end
 
 end
