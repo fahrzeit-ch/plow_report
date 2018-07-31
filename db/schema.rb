9938e400420eb4136e20dbcb7ee8f92400bacf44
Stefan Staub <ste.staub@gmail.com>
2018-07-31 23:56:39 +0200

    fixed rubocop settings in order to pass codeclimate checks again

--- db/schema.rb
+++ db/schema.rb
@@ -101,46 +101,6 @@ ActiveRecord::Schema.define(version: 20180627153725) do
     t.index ["start", "end"], name: "index_drives_on_start_and_end"
   end
 
-  create_table "oauth_access_grants", force: :cascade do |t|
-    t.integer "resource_owner_id", null: false
-    t.bigint "application_id", null: false
-    t.string "token", null: false
-    t.integer "expires_in", null: false
-    t.text "redirect_uri", null: false
-    t.datetime "created_at", null: false
-    t.datetime "revoked_at"
-    t.string "scopes"
-    t.index ["application_id"], name: "index_oauth_access_grants_on_application_id"
-    t.index ["token"], name: "index_oauth_access_grants_on_token", unique: true
-  end
-
-  create_table "oauth_access_tokens", force: :cascade do |t|
-    t.integer "resource_owner_id"
-    t.bigint "application_id"
-    t.string "token", null: false
-    t.string "refresh_token"
-    t.integer "expires_in"
-    t.datetime "revoked_at"
-    t.datetime "created_at", null: false
-    t.string "scopes"
-    t.string "previous_refresh_token", default: "", null: false
-    t.index ["application_id"], name: "index_oauth_access_tokens_on_application_id"
-    t.index ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true
-    t.index ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id"
-    t.index ["token"], name: "index_oauth_access_tokens_on_token", unique: true
-  end
-
-  create_table "oauth_applications", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "uid", null: false
-    t.string "secret", null: false
-    t.text "redirect_uri", null: false
-    t.string "scopes", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
-  end
-
   create_table "recordings", force: :cascade do |t|
     t.datetime "start_time", null: false
     t.bigint "driver_id"
@@ -189,7 +149,5 @@ ActiveRecord::Schema.define(version: 20180627153725) do
   add_foreign_key "driver_logins", "users"
   add_foreign_key "drivers", "companies"
   add_foreign_key "drives", "drivers", name: "fk_drives_driver"
-  add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
-  add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
   add_foreign_key "standby_dates", "drivers", name: "fk_standby_dates_driver"
 end
