11a12acfdec3a884c7c487ebd55cb3b90cb73689
Stefan Staub <ste.staub@gmail.com>
2018-08-16 23:35:27 +0200

    send notification mail when new user signs up #76

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -2,6 +2,14 @@ class User::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+
+  def create
+    super do |resource|
+      AdminMailer.signup_notification_mail.deliver
+    end
+  end
+
+
   protected
 
   def after_sign_up_path_for(resource)
