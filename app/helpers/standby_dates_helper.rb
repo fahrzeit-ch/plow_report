b1dda2b31629db528e39db334c08cb2db1ec127a
Stefan Staub <ste.staub@gmail.com>
2018-09-28 16:15:41 +0200

    replaced a bad color choice

--- app/helpers/standby_dates_helper.rb
+++ app/helpers/standby_dates_helper.rb
@@ -1,9 +1,9 @@
 module StandbyDatesHelper
 
-  COLOR_MAP = %w(#fff #054950 #789A9F #EDF4F5 #FFFFFF #F78F1E #FFEC94 #FFAEAE #FFF0AA #B0E57C #B4D8E7 #56BAEC)
+  COLOR_MAP = %w(#fff #2de9fd #789A9F #EDF4F5 #F78F1E #FFEC94 #FFAEAE #FFF0AA #B0E57C #B4D8E7 #56BAEC)
 
   def driver_color(driver)
-    COLOR_MAP[driver.id % 12]
+    COLOR_MAP[driver.id % 11]
   end
 
 end
