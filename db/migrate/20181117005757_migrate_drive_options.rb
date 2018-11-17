3714578163450e8bca3322935acd888f595e428d
Stefan Staub <ste.staub@gmail.com>
2018-11-17 02:21:07 +0100

    created migration to move data to new drive options

---
+++ db/migrate/20181117005757_migrate_drive_options.rb
@@ -0,0 +1,46 @@
+class MigrateDriveOptions < ActiveRecord::Migration[5.1]
+
+  def self.up
+    plow_id = Activity.find_or_create_by(name: 'Räumen')
+    salting_id = Activity.find_or_create_by(name: 'Salzen')
+    refill_id = Activity.find_or_create_by name: 'Salz füllen' do |a|
+      a.has_value = true
+      a.value_label = 'Salzmenge'
+    end
+
+    Company.all.each do |c|
+      plow = c.activities.find_or_create_by(name: 'Räumen')
+      salt = c.activities.find_or_create_by(name: 'Salzen')
+      refill = c.activities.find_or_create_by name: 'Salz füllen' do |a|
+        a.has_value = true
+        a.value_label = 'Salzmenge'
+      end
+
+      c.drives.each do |drive|
+        if drive.salt_refilled
+          values = {activity_id: refill.id, value: drive.salt_amount_tonns }
+        elsif drive.plowed
+          values = {activity_id: plow.id }
+        elsif drive.salted
+          values = {activity_id: salt.id }
+        end
+        drive.update(activity_execution_attributes: values) unless values.blank?
+      end
+    end
+
+    Drive.joins(:driver).where(drivers: {company_id: nil}).find_each do |drive|
+
+      if drive.salt_refilled
+        values = {activity_id: refill_id, value: drive.salt_amount_tonns }
+      elsif drive.plowed
+        values = {activity_id: plow_id }
+      elsif drive.salted
+        values = {activity_id: salting_id }
+      end
+      drive.update(activity_execution_attributes: values) unless values.blank?
+    end
+  end
+
+  def self.down
+  end
+end
