59c48b37a67dc75b620238f24f4704d763a6952b
Stefan Staub <ste.staub@gmail.com>
2018-12-06 01:08:07 +0100

    minor refactoring

--- app/helpers/drive_form_builder.rb
+++ app/helpers/drive_form_builder.rb
@@ -16,13 +16,30 @@ class DriveFormBuilder < BootstrapFormBuilder
   end
 
   def activity_value
+    options = append_classes({min: 0, step: 0.01, id: :activity_value_field}, :'activity_execution.value')
 
+    fields_for :activity_execution, activity_execution_association do |f|
+      f.label(:value, activity_value_label_text, id: 'activity_value_label') +
+          f.number_field(:value, options)
+    end
+  end
+
+  def activity_value_collapsed?
+    activity_object.try(:has_value?)
   end
 
   private
 
   def activity_execution_association
     object.activity_execution || object.build_activity_execution
+  end
+
+  def activity_value_label_text
+    activity_object.try(:value_label)
+  end
+
+  def activity_object
+    object.activity_execution.try(:activity)
   end
 
   def value_field_data(activity)
