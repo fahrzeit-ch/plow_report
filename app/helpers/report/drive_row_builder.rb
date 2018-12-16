c22ceb705042d1572325d565c3a873d6d7f0c040
Stefan Staub <ste.staub@gmail.com>
2018-12-17 00:46:49 +0100

    improvements to excel report

---
+++ app/helpers/report/drive_row_builder.rb
@@ -0,0 +1,30 @@
+module Report
+  class DriveRowBuilder
+    ACTIVITY_EXECUTION_INDICATOR_VALUE = 'x'
+
+    # @param [Report::Styles] styles
+    # @param [Hash] activity_index_map
+    def initialize(styles, activity_index_map)
+      @activity_index_map = activity_index_map
+      @styles = styles
+    end
+
+    # @param [Drive] drive
+    def columns_for(drive)
+      columns = [
+        drive.start, drive.start, drive.duration, drive.driver.name, drive.distance_km
+      ]
+      return columns unless drive.activity_execution
+
+      ae = drive.activity_execution
+      idx = @activity_index_map[ae.activity_id]
+      values = [ACTIVITY_EXECUTION_INDICATOR_VALUE]
+      values << ae.value if ae.activity.has_value?
+      columns.insert(idx, *values)
+    end
+
+    def styles
+      [@styles.date, @styles.time, @styles.duration]
+    end
+  end
+end
---
+++ app/helpers/report/styles.rb
@@ -0,0 +1,35 @@
+module Report
+  class Styles
+    attr_accessor :header
+    attr_accessor :header_vertical
+    attr_accessor :date
+    attr_accessor :duration
+    attr_accessor :time
+    attr_accessor :h1
+    attr_accessor :h2
+    attr_accessor :checked
+
+    # @param [Axlsx::Workbook] workbook
+    def initialize(workbook)
+      @workbook = workbook
+      build_styles
+    end
+
+    def build_styles
+      @h1 = styles.add_style(sz: 15, b: true, u: true)
+      @h1 = styles.add_style(sz: 13, b: true, u: true)
+
+      @header = styles.add_style(bg_color: '1a6ba2', fg_color: 'FF', b: true)
+      @header_vertical = styles.add_style(b: true, alignment: { textRotation: 90 }, bg_color: '1a6ba2', fg_color: 'FF')
+
+      @time = styles.add_style format_code: 'hh:mm'
+      @duration = styles.add_style format_code: 'hh:mm'
+      @date = Axlsx::STYLE_DATE
+      @checked = styles.add_style alignment: { horizontal: :center }
+    end
+
+    def styles
+      @workbook.styles
+    end
+  end
+end
