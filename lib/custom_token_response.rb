eab72701e4be43f01127feafd26078f314b33d27
Stefan Staub <stefan.staub@pta.ch>
2019-10-17 00:48:06 +0200

    fixed missing default_company_id in token response

--- lib/custom_token_response.rb
+++ lib/custom_token_response.rb
@@ -1,8 +1,10 @@
 module CustomTokenResponse
   def body
+    owner_id = @token.resource_owner_id
+    company_id = CompanyMember.select(:company_id).find_by(user_id: owner_id).try(:company_id)
     additional_data = {
-        'userid' => @token.resource_owner_id.to_s,
-        'default_company_id' => CompanyMember.select(:company_id).find_by(user_id: 2).try(:company_id),
+        'userid' => owner_id.to_s,
+        'default_company_id' => company_id.to_s
         'created_at' => @token.created_at.to_i.to_s
     }
     # call original `#body` method and merge its result with the additional data hash
