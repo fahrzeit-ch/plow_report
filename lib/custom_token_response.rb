f06460621ab7ce5123ab509dac7b4ab9e6a84041
Stefan Staub <stefan.staub@pta.ch>
2019-10-17 00:55:13 +0200

    fixed syntax error

--- lib/custom_token_response.rb
+++ lib/custom_token_response.rb
@@ -4,7 +4,7 @@ module CustomTokenResponse
     company_id = CompanyMember.select(:company_id).find_by(user_id: owner_id).try(:company_id)
     additional_data = {
         'userid' => owner_id.to_s,
-        'default_company_id' => company_id.to_s
+        'default_company_id' => company_id.to_s,
         'created_at' => @token.created_at.to_i.to_s
     }
     # call original `#body` method and merge its result with the additional data hash
