f664e813c346e86528f08ed8e740de533e927fab
Stefan Staub <stefan.staub@pta.ch>
2020-10-27 16:57:13 +0100

    use new rails 5.2 defaults

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  protect_from_forgery with: :exception
-
   helper_method :current_season
   helper_method :selected_season
   helper_method :current_driver
