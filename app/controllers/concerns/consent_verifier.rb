56ad3316291f2d59e46560ea45427354e0856d0d
Stefan Staub <stefan.staub@pta.ch>
2020-10-30 16:00:00 +0100

    store original url before redirecting in term acceptance check #281

--- app/controllers/concerns/consent_verifier.rb
+++ app/controllers/concerns/consent_verifier.rb
@@ -4,6 +4,7 @@ module ConsentVerifier
   def check_consents
     return unless current_user
     if current_user.force_consent_required?
+      store_location_for(:user, request.original_url)
       redirect_to edit_term_acceptances_path
     end
   end
