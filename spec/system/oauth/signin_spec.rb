f2f10a8bdcab24c635ac9a2884ffa53c874a20ee
Stefan Staub <stefan.staub@pta.ch>
2020-11-02 09:21:51 +0100

    fix rubocop offenses

--- spec/system/oauth/signin_spec.rb
+++ spec/system/oauth/signin_spec.rb
@@ -1,34 +1,36 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 feature "authenticate via oauth" do
-  let!(:app) { Doorkeeper::Application.create!(name: 'test', scopes: 'profile email phone address', confidential: false, redirect_uri: 'https://www.example.com', default_app: true)}
+  let!(:app) { Doorkeeper::Application.create!(name: "test", scopes: "profile email phone address", confidential: false, redirect_uri: "https://www.example.com", default_app: true) }
 
   describe "login with existing user" do
-    let(:password) { 'password' }
+    let(:password) { "password" }
     let!(:user) { create(:user, password: password, password_confirmation: password) }
 
     it "will show signin page" do
-      query = URI.encode_www_form({ response_type: 'code', state: '', client_id: app.uid, scope: "profile email phone address", redirect_uri: app.redirect_uri })
+      query = URI.encode_www_form({ response_type: "code", state: "", client_id: app.uid, scope: "profile email phone address", redirect_uri: app.redirect_uri })
       visit "/oauth/authorize?#{query}"
       expect(page).to have_content("E-Mail")
       expect(page).to have_content("Passwort")
     end
 
-    it 'signing in will redirect to redirect_uri' do
-      query = URI.encode_www_form({ response_type: 'code', state: '', client_id: app.uid, scope: "profile email phone address", redirect_uri: app.redirect_uri })
+    it "signing in will redirect to redirect_uri" do
+      query = URI.encode_www_form({ response_type: "code", state: "", client_id: app.uid, scope: "profile email phone address", redirect_uri: app.redirect_uri })
       visit "/oauth/authorize?#{query}"
-      fill_form 'user', { email: user.email, password: password }
-      click_button I18n.t('devise.sign_in')
+      fill_form "user", { email: user.email, password: password }
+      click_button I18n.t("devise.sign_in")
       expect(current_url).to start_with(app.redirect_uri)
     end
 
     context "with terms not accepted" do
       let!(:term) { create(:policy_term, required: true) }
       it "redirects to accept terms" do
-        query = URI.encode_www_form({ response_type: 'code', state: '', client_id: app.uid, scope: "profile email phone address", redirect_uri: app.redirect_uri })
+        query = URI.encode_www_form({ response_type: "code", state: "", client_id: app.uid, scope: "profile email phone address", redirect_uri: app.redirect_uri })
         visit "/oauth/authorize?#{query}"
-        fill_form 'user', { email: user.email, password: password }
-        click_button I18n.t('devise.sign_in')
+        fill_form "user", { email: user.email, password: password }
+        click_button I18n.t("devise.sign_in")
 
         expect(page).to have_content(term.name)
         check "user[terms][agb]"
@@ -38,26 +40,25 @@ feature "authenticate via oauth" do
     end
   end
 
-  describe 'sign up with new user' do
-    let(:password) { 'password' }
+  describe "sign up with new user" do
+    let(:password) { "password" }
     let(:user) { attributes_for(:user) }
 
     it "will show signin page" do
-      query = URI.encode_www_form({ response_type: 'code', state: '', client_id: app.uid, scope: "profile email phone address", redirect_uri: app.redirect_uri })
+      query = URI.encode_www_form({ response_type: "code", state: "", client_id: app.uid, scope: "profile email phone address", redirect_uri: app.redirect_uri })
       visit "/oauth/authorize?#{query}"
-      expect(page).to have_link(I18n.t('devise.sign_up'))
+      expect(page).to have_link(I18n.t("devise.sign_up"))
     end
 
-    it 'signing in will redirect to redirect_uri' do
-      query = URI.encode_www_form({ response_type: 'code', state: '', client_id: app.uid, scope: "profile email phone address", redirect_uri: app.redirect_uri })
+    it "signing in will redirect to redirect_uri" do
+      query = URI.encode_www_form({ response_type: "code", state: "", client_id: app.uid, scope: "profile email phone address", redirect_uri: app.redirect_uri })
       visit "/oauth/authorize?#{query}"
-      within '#navbarSupportedContent' do
-        click_link I18n.t('devise.sign_up')
+      within "#navbarSupportedContent" do
+        click_link I18n.t("devise.sign_up")
       end
-      fill_form 'user', { name: user[:name], email: user[:email], password: password, password_confirmation: password }
-      click_button I18n.t('devise.sign_up')
+      fill_form "user", { name: user[:name], email: user[:email], password: password, password_confirmation: password }
+      click_button I18n.t("devise.sign_up")
       expect(current_url).to start_with(app.redirect_uri)
     end
   end
-
 end
