6855cca8b13edbb843394a51f0c27f62aab0487e
Stefan Staub <stefan.staub@pta.ch>
2021-02-01 11:37:23 +0100

    Add date_range field to tours_report

--- spec/models/tours_report_spec.rb
+++ spec/models/tours_report_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ToursReport, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to(:date_range) }
+
+  describe "date range" do
+    subject { described_class.new(date_range: "2021-01-01T00:00 - 2021-01-31T23:59") }
+    its(:start_date) { is_expected.to eq(DateTime.parse("2021-01-01T00:00")) }
+    its(:end_time) { is_expected.to eq(DateTime.parse("2021-01-31T23:59")) }
+  end
 end
