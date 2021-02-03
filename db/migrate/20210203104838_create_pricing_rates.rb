eeb946cce470e3cdf16657f53a669f43d2db26dd
Stefan Staub <stefan.staub@pta.ch>
2021-02-03 19:34:51 +0100

    Change valid_from columns to date

--- db/migrate/20210203104838_create_pricing_rates.rb
+++ db/migrate/20210203104838_create_pricing_rates.rb
@@ -1,10 +1,10 @@
-class CreatePricingHourlyRates < ActiveRecord::Migration[5.2]
+class CreatePricingRates < ActiveRecord::Migration[5.2]
   def change
     create_table :pricing_hourly_rates do |t|
       t.references :hourly_ratable, polymorphic: true, index: { name: "index_hourly_rates_on_hourly_priceable_id" }
       t.integer :price_cents, null: false, default: 0
       t.string :price_currency, null: false
-      t.datetime :valid_from, null: false
+      t.date :valid_from, null: false
 
       t.timestamps
     end
@@ -13,7 +13,7 @@ class CreatePricingHourlyRates < ActiveRecord::Migration[5.2]
       t.references :flat_ratable, polymorphic: true, index: { name: "index_flat_rates_on_rable_id_ratable_type" }
       t.integer :price_cents, null: false, default: 0
       t.string :price_currency, null: false
-      t.datetime :valid_from, null: false
+      t.date :valid_from, null: false
       t.string :rate_type, null: false, default: Pricing::FlatRate::CUSTOM_FEE
 
       t.timestamps
