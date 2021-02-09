a483fa5b8df3a61b56a655847d66788faafd668f
Stefan <ste.staub@gmail.com>
2021-02-09 15:20:07 +0100

    prevent activity_assignments from beeing destroyed if used #353

--- app/models/vehicle_activity_assignment.rb
+++ app/models/vehicle_activity_assignment.rb
@@ -6,9 +6,24 @@ class VehicleActivityAssignment < ApplicationRecord
   belongs_to :vehicle
   belongs_to :activity
 
-  accepts_nested_attributes_for :activity, reject_if: :all_blank
+  accepts_nested_attributes_for :activity, reject_if: :all_blank, allow_destroy: false
+  validates_uniqueness_of :activity_id, scope: :vehicle_id
+
+  before_destroy :check_can_destroy
 
   def pricing_default_valid_from
     Season.current.start_date
   end
+
+  def can_destroy?
+    !Drive.joins(:activity_execution).where(activity_executions: {activity_id: activity_id }, vehicle: vehicle).any?
+  end
+
+  private
+    def check_can_destroy
+      unless can_destroy?
+        errors.add :base, :dont_destroy_dependent_data
+        throw :abort
+      end
+    end
 end
