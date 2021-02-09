d2166ba9596e844358fc2de022a2e1c694a4ae8a
Stefan <ste.staub@gmail.com>
2021-02-09 09:42:04 +0100

    Ensure only one activity flatrate per site and activity #319

--- app/models/site_activity_flat_rate.rb
+++ app/models/site_activity_flat_rate.rb
@@ -4,6 +4,8 @@ class SiteActivityFlatRate < ApplicationRecord
   belongs_to :site
   belongs_to :activity
 
+  validates_uniqueness_of :activity_id, scope: :site_id
+
   include Pricing::FlatRatable
   flat_rate :activity_fee
 end
