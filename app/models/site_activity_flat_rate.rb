2c665ba7003b7ae7d8bf340794e55df16d922fac
Stefan <ste.staub@gmail.com>
2021-02-09 10:38:49 +0100

    make sure not to destroy site activity flatrates when drives are depending on them #319

--- app/models/site_activity_flat_rate.rb
+++ app/models/site_activity_flat_rate.rb
@@ -5,7 +5,22 @@ class SiteActivityFlatRate < ApplicationRecord
   belongs_to :activity
 
   validates_uniqueness_of :activity_id, scope: :site_id
+  before_destroy :check_can_destroy
 
   include Pricing::FlatRatable
   flat_rate :activity_fee, defaults: { active: true }
+
+  def can_destroy?
+    if persisted?
+      !Drive.joins(:activity_execution).where(site_id: site_id, activity_executions: {activity_id: activity_id }).any?
+    end
+  end
+
+  private
+    def check_can_destroy
+      unless can_destroy?
+        errors.add :base, :dont_destroy_dependent_data
+        throw :abort
+      end
+    end
 end
--- spec/factories/site_activity_flat_rates.rb
+++ spec/factories/site_activity_flat_rates.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :site_activity_flat_rate do
-    site { nil }
-    activity { nil }
+    site { association :site }
+    activity { association :activity }
+    activity_fee_attributes { { active: true, valid_from: Season.current.start_date, price: "20" } }
   end
 end
