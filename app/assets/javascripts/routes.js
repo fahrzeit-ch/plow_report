35a40db54995b7a0e23d3d52d6e12c83699d4a44
Stefan <ste.staub@gmail.com>
2021-08-24 21:46:36 +0200

    experimenting with drag and drop

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,22 +1,34 @@
 $(document).on('turbolinks:load', function() {
-   const selected_sites = document.getElementById('selected_sites');
-   const available_sites = document.getElementById('available_sites');
+    const selected_sites = document.getElementById('selected_sites');
+    const available_sites = document.getElementById('available_sites');
 
-   const selected_sortable = Sortable.create(selected_sites, {
+    const selected_sortable = Sortable.create(selected_sites, {
         group: "shared",
         ghostClass: 'drop-ghost',
         filter: '.no-drag',
+        dataIdAttr: 'data-id',
         onAdd: (event) => {
-
+            hideInfoTargetList();
+            createFormElement(event);
         },
         onRemove: (event) => {
-
+            updateInfoStateTargetList();
         }
     });
+
+
     const available_sortable = Sortable.create(available_sites, {
         group: { name: "shared"},
         sort: false
     })
 
+    function updateInfoStateTargetList() {
+        if(selected_sortable.toArray().length === 0) {
+            $('.drag-target-info').show();
+        }
+    }
 
+    function hideInfoTargetList() {
+        $('.drag-target-info').hide();
+    }
 });
