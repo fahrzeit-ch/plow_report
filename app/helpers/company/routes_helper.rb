def36239bef99c3b730f29657bce402004f5beb2
Stefan <ste.staub@gmail.com>
2021-08-31 23:16:42 +0200

    add default driving route attribute to vehicle form field

--- app/helpers/company/routes_helper.rb
+++ app/helpers/company/routes_helper.rb
@@ -5,4 +5,8 @@ module Company::RoutesHelper
     Site.joins(:customer).where(customer: { client_of: company}).active.order(:display_name)
   end
 
+  def available_driving_routes
+    DrivingRoute.kept.order(:name).where(company_id: current_company.id)
+  end
+
 end
