b7c8c5fe60235c6582d221585ec6f828b479b215
stefan <Stefan.Staub@pta.ch>
2023-12-23 12:29:47 +0100

    fix migration

--- db/migrate/20231223111451_update_drives_with_pricings_to_version_2.rb
+++ db/migrate/20231223111451_update_drives_with_pricings_to_version_2.rb
@@ -1,5 +1,5 @@
 class UpdateDrivesWithPricingsToVersion2 < ActiveRecord::Migration[6.1]
   def change
-    update_view :drives_with_pricings, version: 2, revert_to_version: 1
+    update_view :drives_with_pricings, version: 2, revert_to_version: 1, materialized: true
   end
 end
